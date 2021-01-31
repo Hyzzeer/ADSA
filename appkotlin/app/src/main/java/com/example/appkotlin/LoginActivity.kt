34ff1abad83238fe9399ef1e72a1df8d4d46eae8
lucaslndr <lucas.landru@isen.yncrea.fr>
2021-01-31 01:15:54 +0100

    login onclick connexion

--- appkotlin/app/src/main/java/com/example/appkotlin/LoginActivity.kt
+++ appkotlin/app/src/main/java/com/example/appkotlin/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.appkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import com.android.volley.*
 import com.android.volley.toolbox.*
@@ -33,39 +34,75 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    var TAG = "LOGIN_MESSAGE"
+    private var TAG:String = "LOGIN_MESSAGE"
+    private var ERROR_FETCH:String = "error fetch"
 
-    fun connexionListener(button:Button){
-        findUsernameById(usernameInput.text.toString())
+    fun onClickConnexion(button:View){
+        var user:JSONObject? = checkCredentialsOnline(nameInput.text.toString(), lastnameInput.text.toString())
+        if ((user!=null)){
+            loginMessage.text = "Good"
+            // save user info dans bdd
+            // changer activity
+        }
+        else if (user==null){
+            loginMessage.text = "Wrong credentials"
+        }
     }
 
-    fun findUsernameById(username:String){
+    val url_config = "https://6007f1a4309f8b0017ee5022.mockapi.io/api/m1/config/1"
+    val url_accounts = "https://6007f1a4309f8b0017ee5022.mockapi.io/api/m1/accounts/"
+
+    // check user credentials, return user:JSONObject if ok, else return null
+    private fun checkCredentialsOnline(name:String, lastname:String):JSONObject?{
         val queue = Volley.newRequestQueue(this)
-        val url_config = "https://6007f1a4309f8b0017ee5022.mockapi.io/api/m1/congif/"
-        val url_accounts = "https://6007f1a4309f8b0017ee5022.mockapi.io/api/m1/accounts/"
+
+        var user:JSONObject? = null
+        var ok:Boolean = false
+
+        val stringRequest = StringRequest(Request.Method.GET, url_config,
+                Response.Listener<String>{ response ->
+                    val resStr:String = response.toString()
+                    user = JSONObject(resStr)
+                    ok = (user!!.get(name).toString()==name) and (user!!.get(name).toString()==name)
+                },
+                Response.ErrorListener { error ->
+                    Log.i(TAG, error.toString())
+                    loginMessage.text = ERROR_FETCH
+                })
+        queue.add(stringRequest)
+        queue.start()
+        return if (ok) user
+        else null
+    }
+
+    private fun findAccountsOnline():JSONArray{
+        val queue = Volley.newRequestQueue(this)
+
+        lateinit var accounts: JSONArray
 
         val stringRequest = StringRequest(Request.Method.GET, url_accounts,
                 Response.Listener<String> { response ->
-                    var resStr = response.toString()
-                    val accounts: JSONArray = JSONArray(resStr)
-                    var i = 0
-                    var found:Boolean = false
-                    while((i<accounts.length()) and (!found)){
-                        var account: JSONObject = accounts.getJSONObject(i)
-                        if (account.get("account_name").toString() == usernameInput.text.toString()){
-                            // put user in bdd
-                            // navigate to main activity
-                            found=true
-                        }
-                        i+=1
-                    }
-                    if (found) infoText.text="FOUND"
-                    else infoText.text="NOT FOUND"
+                    val resStr:String = response.toString()
+                    accounts = JSONArray(resStr)
                 },
                 Response.ErrorListener {
-                    infoText.text="ERROR FETCH"
                     Log.i(TAG, "error fetch")
                 })
         queue.add(stringRequest)
+        return accounts
+    }
+
+    fun findAccountsByAccountNameOnline(account_name:String): JSONArray {
+        val accounts:JSONArray = findAccountsOnline()
+        lateinit var accountsFound: JSONArray
+        var i = 0
+        while((i<accounts.length())){
+            var account: JSONObject = accounts.getJSONObject(i)
+            if (account.get("account_name").toString() == account_name){
+                accountsFound.put(account)
+            }
+            i+=1
+        }
+        return accountsFound
     }
 }
